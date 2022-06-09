9292d80008d4df2fa6266b94ca9c8154f143e509
Kcisti <91692567+Kcisti@users.noreply.github.com>
2022-06-09 22:33:28 +0200

    899869697576985867897998758579

--- script.js
+++ script.js
@@ -1,175 +1,199 @@
 const DOM = {
-  contMex: document.querySelector('#messages_container'),
-  botMex: document.querySelector('.bot-messages'),
-  userMex: document.querySelector('.user-messages'),
-  mic: document.querySelector('.mic-content'),
+  //Elements to client
+  menuIcon: document.querySelector('.nav_menu'),
+  menuIconUn: document.querySelector('.nav_menuUn'),
+  home : document.getElementById('home'),
+  aboutMe : document.getElementById('aboutMe'),
+  skills : document.getElementById('skills'),
+  contact : document.getElementById('contact'),
+  //Elements to server
+  messages: document.querySelector('.things'),
+  input: document.querySelector('.message-form__input'),
+  form: document.querySelector('.message-form'),
+  area: document.querySelector('.nav_title'),
+};
+
+
+//Client Settings
+DOM.menuIconUn.style.display = 'none';
+
+function menuVisible(){
+  var menuStatus = 'visible';
+  window.scrollTo( {
+    top : 0,
+    left : 0,
+    behavior: 'smooth'
+  });
+  setTimeout(function(){
+    document.getElementById("navMenu").style.height="700px";
+    document.getElementById("navAnima").style.transform="translateY(0px)";
+    DOM.menuIcon.style.display = 'none';
+    DOM.menuIconUn.style.display = 'block';
+    console.log(menuStatus);
+  },300)
+}
+DOM.menuIcon.addEventListener('click', menuVisible);
+
+function menuUnvisible(){
+  var menuStatus = 'unvisible';
+  document.getElementById("navMenu").style.height="0px";
+  document.getElementById("navAnima").style.transform="translateY(-700px)";
+  DOM.menuIconUn.style.display = 'none';
+  DOM.menuIcon.style.display = 'block';
+  console.log(menuStatus);
+}
+DOM.menuIconUn.addEventListener('click', menuUnvisible);
+
+document.getElementById('WSTUDIO').style.display="none";
+
+DOM.home.addEventListener('click',TOhome);
+DOM.aboutMe.addEventListener('click',TOaboutUs);
+DOM.skills.addEventListener('click',TOskills);
+DOM.contact.addEventListener('click',TOcontact);
+
+
+function TOhome(){
+  menuUnvisible();
+  setTimeout(function(){
+    window.scrollTo( {
+      top : 0,
+      left : 0,
+      behavior: 'smooth'
+    });
+  },1000)
+}
+function TOaboutUs(){
+  menuUnvisible();
+  setTimeout(function(){
+    window.scrollTo( {
+      top : 1600,
+      left : 0,
+      behavior: 'smooth'
+    });
+  },1000)
+}
+function TOskills(){
+  menuUnvisible();
+  setTimeout(function(){
+    window.scrollTo( {
+      top : 3200,
+      left : 0,
+      behavior: 'smooth'
+    });
+  },1000)
+}
+function TOcontact(){
+  menuUnvisible();
+  setTimeout(function(){
+    window.scrollTo( {
+      top : 4300,
+      left : 0,
+      behavior: 'smooth'
+    });
+  },1000)
 }
 
-DOM.userMex.style.display = 'none';
-DOM.mic.style.display = 'none';
-
-function startMessage(){
-  const text_speak = new SpeechSynthesisUtterance();
-  text_speak.lang = 'it-IT';
-  voices = speechSynthesis.getVoices();
-  console.log(voices);
-  DOM.botMex.innerHTML = 'INIT';
+window.addEventListener('onload',HeightRulesLoad());
+function HeightRulesLoad(){
+  window.scrollTo( {
+    top : 0,
+    left : 0,
+    behavior: 'smooth'
+  });
 }
 
-function starTalk(){
-  DOM.botMex.style.display = 'none';
-  DOM.userMex.style.display = 'block';
-  DOM.mic.style.display = 'block';
-  const text_speak = new SpeechSynthesisUtterance();
-  text_speak.lang = 'it-IT';
-  text_speak.rate = 1;
-  text_speak.pitch = '1';
-  text_speak.voice = speechSynthesis.getVoices()[11];
 
-  var day = new Date();
-  var hr = day.getHours();
+//Server Settings
+const CLIENT_ID = 'EFslC1FFGukiWwqQ';
+const drone = new ScaleDrone(CLIENT_ID, {
+  data: { name: 'Kcisti'}
+});
+drone.on('open', error => {
+  if (error) {
+    return console.error(error);
+  }
+  console.log('Successfully connected to Scaledrone');
 
-  if(hr >= 0 && hr < 12) {
-    text_speak.text = 'buongiorno capo';
-    window.speechSynthesis.speak(text_speak);
-    jQuery(DOM.contMex).append("<p class='bot-messages'>Buongiorno capo</p>");
-  }
-  else if(hr >= 12 && hr < 13) {
-    text_speak.text = "salve capo";
-    window.speechSynthesis.speak(text_speak);
-    jQuery(DOM.contMex).append("<p class='bot-messages'>Salve capo</p>");
-  }
-  else if(hr >= 13 && hr < 19) {
-    text_speak.text = 'buon pomeriggio capo';
-    window.speechSynthesis.speak(text_speak);
-    jQuery(DOM.contMex).append("<p class='bot-messages'>Buon pomeriggio capo</p>");
-  }
-  else if(hr >= 19 && hr < 22) {
-    text_speak.text = 'buona sera capo';
-    window.speechSynthesis.speak(text_speak);
-    jQuery(DOM.contMex).append("<p class='bot-messages'>Buona sera capo</p>");
-  }
-  else if(hr >= 22) {
-    text_speak.text = 'buona notte capo';
-    window.speechSynthesis.speak(text_speak);
-    jQuery(DOM.contMex).append("<p class='bot-messages'>Buona notte capo</p>");
-  }
+  const room = drone.subscribe('observable-room');
+  room.on('open', error => {
+    if (error) {
+      return console.error(error);
+    }
+    console.log('Successfully joined room');
+  });
+
+  room.on('data', (text,) => {
+      addMessageToListDOM(text);
+  });
+});
+
+function openStudio(){
+  document.getElementById('WSTUDIO').style.display="block";
+  document.getElementById('check').style.display="block";
+  document.getElementById('room').style.display="none";
+  document.getElementById('message_form').style.display="none";
 }
-
-window.addEventListener('click',startMessage)
-DOM.botMex.addEventListener('click',starTalk)
-
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
-
-recognition.onresult = (event) => {
-    const current = event.resultIndex;
-    const transcript = event.results[current][0].transcript;
-    DOM.userMex.textContent = transcript;
-    speakThis(transcript.toLowerCase());
+function closeStudio(){
+  document.getElementById('WSTUDIO').style.display="none";
+  document.getElementById('room').style.display="block";
 }
+DOM.area.addEventListener('click', openStudio);
 
-DOM.mic.addEventListener('click', ()=>{
-    recognition.start();
-})
-
-function speakThis(message) {
-  if(!navigator.geolocation) {
-    return false;
+function accessGranted(){
+  var pin = document.getElementById("checkpin").value;
+  if (pin === '2321222425') {
+    document.getElementById('check_resultTitle').style.color="#343A90";
+    document.getElementById('check_resultTitle').innerText="ACCESS GRANTED";
+    setTimeout(function(){
+      document.getElementById('check').style.display="none";
+      document.getElementById('message_form').style.display="block";
+    },700)
   } else {
-    navigator.geolocation.getCurrentPosition(success);
+    document.getElementById('check_resultTitle').style.color="red";
+    document.getElementById('checkpin').style.border="1px solid red";
+    document.getElementById('check_resultTitle').innerText="ACCESS DENIED";
+
   }
+}
 
-  function success(position) {
-    const latitude  = position.coords.latitude;
-    const longitude = position.coords.longitude;
+function sendMessage() {
+  const value = DOM.input.value;
+  if (value === '') {
+    return;
   }
-  const speech = new SpeechSynthesisUtterance();
-  speech.lang = 'it-IT';
-  speech.rate = 1;
-  speech.pitch = '1';
-  speech.voice = speechSynthesis.getVoices()[11];
+  DOM.input.value = '';
+  drone.publish({
+    room: 'observable-room',
+    message: value,
+  });
+  closeStudio();
+}
+DOM.form.addEventListener('click', sendMessage);
 
-    speech.text = "Non ti sento, Riprova";
-
-    //Personal
-
-    if(message.includes('ciao')) {
-        const finalText = "ti ascolto capo";
-        speech.text = finalText;
-    }
-
-    else if(message.includes('come va')|| message.includes('come stai')) {
-        const finalText = "tutto bene capo";
-        speech.text = finalText;
-    }
-
-    else if(message.includes('tuo nome')) {
-        const finalText = "Sono Luna";
-        speech.text = finalText;
-    }
-
-    else if(message.includes('anni hai')) {
-        const finalText = "io non ho. sono la tua assistente vocale";
-        speech.text = finalText;
-    }
-
-    else if(message.includes('sono angela bos')|| message.includes('23-25')) {
-        const finalText = "Ti conosco, il mio capo mi parla sempre di te. Dice che sei magnifica.";
-        speech.text = finalText;
-    }
-
-    //util functions
-
-    else if(message.includes('google')) {
-        window.open("https://google.com", "_blank");
-        const finalText = "Lo apro";
-        speech.text = finalText;
-    }
-
-    else if(message.includes('instagram')) {
-        window.open("https://instagram.com", "_blank");
-        const finalText = "Lo apro";
-        speech.text = finalText;
-    }
-
-    else if(message.includes('wikipedia')) {
-        window.open(`https://en.wikipedia.org/wiki/${message.replace("wikipedia", "")}`, "_blank");
-        const finalText = "Apro" + message;
-        speech.text = finalText;
-    }
-
-    else if(message.includes('meteo')) {
-      window.open(`https://www.google.com/search?q=meteo`, "_blank");
-        const finalText = 'Lo cerco';
-        speech.text = finalText;
-    }
-
-    else if(message.includes('ora')) {
-        const timeHour = new Date().toLocaleString(undefined, {hour: "numeric"})
-        const timeMinute = new Date().toLocaleString(undefined, {minute: "numeric"})
-        const finalText = 'sono le ' + timeHour + ' e '+ timeMinute + ' minuti';
-        speech.text = finalText;
-    }
-
-    else if(message.includes('data')) {
-        const date = new Date().toLocaleString(undefined, {month: "long", day: "numeric"})
-        const year = new Date().toLocaleString(undefined, {year: "numeric"})
-        const finalText = 'oggi, il ' + date + " dell'anno " + year;
-        speech.text = finalText;
-    }
-    else if(message.includes('mia posizione')) {
-      const finalText = 'sono a ' + latitude + ' gradi di latitudine e '+ longitude + ' di longitudine';
-      speech.text = finalText;
-    }
-
-
-    else {
-        const finalText = "non so aiutarti perdonami";
-        speech.text = finalText;
-    }
-
-    window.speechSynthesis.speak(speech);
-
-    jQuery(DOM.contMex).append("<p class='bot-messages'>"+ speech.text +"</p>");
+function addZero(i){
+  if (i < 10) {
+    i = "0" + i;
+  } return i;
+}
+function createMessageElement(text) {
+  var numero = Math.floor(Math.random()*10000);
+  var postid = 'post' + numero;
+  jQuery("#things").append("<div class='post' id='"+ postid +"'></div>");
+  var select = document.getElementById("cat");
+  var topic = select.options[select.selectedIndex].value;
+  jQuery("#"+ postid).append("<p class='things_topic'>"+ topic +"</p>");
+  var el = document.createTextNode(text);
+  jQuery("#"+ postid).append("<p class='things_write'>"+ el.wholeText +"</p>");
+  var d = new Date();
+  var date = d.getDate()+'/'+d.getMonth()+'/'+d.getFullYear();
+  var ora = addZero(d.getHours());
+  var min = addZero(d.getMinutes());
+  jQuery("#"+ postid).append("<p class='things_name'>Kcisti</p>");
+  jQuery("#"+ postid).append("<p class='things_dates'>" + date + "</p>");
+  jQuery("#"+ postid).append("<ion-icon name='time-outline' class='things_hourI'></ion-icon>");
+  jQuery("#"+ postid).append("<p class='things_hour'>"+ ora +":"+ min +"</p>");
+}
+function addMessageToListDOM(text) {
+  const content = DOM.messages;
+  content.append(createMessageElement(text));
 }
