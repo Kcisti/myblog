dbf5a7a4ee951b8a7c7fb2769e997e2f74f8f12f
Kcisti <91692567+Kcisti@users.noreply.github.com>
2022-06-22 23:57:10 +0200

    59689706870987607

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const DOM = {
+  //buttons
   homeTitle : document.querySelector('#homeTitle'),
   iconMenu : document.querySelector('#iconMenu'),
+  menuEsc : document.querySelector('.menu_esc'),
+  menuHome : document.getElementById('menHome'),
+  menuAboutMe : document.getElementById('menAboutMe'),
+  menuSkills : document.getElementById('menSkills'),
+  menuContact : document.getElementById('menContact'),
+
+  //divs
+  menu : document.querySelector('.menu'),
+  visible : document.querySelector('.visible'),
 }
 
 function message(){
@@ -17,9 +27,39 @@ function message(){
     DOM.homeTitle.innerText = 'Good Evening';
   }
 }
+
 function switchTOmenu(){
-  document.querySelector('.menu').style.transform = 'translateX(0px)';
+  DOM.menu.style.transform = 'translateX(0px)';
+  DOM.visible.style.transform = 'translateX(800px)';
+  DOM.visible.style.backgroundColor = '#8F8F8F';
+  console.log('menu on');
+}
+function closeMenu(){
+  DOM.menu.style.transform = 'translateX(-800px)';
+  DOM.visible.style.transform = 'translateX(0px)';
+  DOM.visible.style.backgroundColor = '#D2D2D2';
+  console.log('menu off');
 }
 
 window.addEventListener('load', message);
 DOM.iconMenu.addEventListener('click', switchTOmenu);
+DOM.menuEsc.addEventListener('click', closeMenu);
+
+
+function TOhome(){
+
+}
+function TOaboutUs(){
+
+}
+function TOskills(){
+
+}
+function TOcontact(){
+
+}
+
+DOM.menuHome.addEventListener('click',TOhome);
+DOM.menuAboutMe.addEventListener('click',TOaboutUs);
+DOM.menuSkills.addEventListener('click',TOskills);
+DOM.menuContact.addEventListener('click',TOcontact);
