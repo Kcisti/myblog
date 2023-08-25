81ffc3dec539168df8b350162fba334dfbd405b5
Kcisti <91692567+Kcisti@users.noreply.github.com>
2023-08-25 21:14:41 +0200

    8879869757587586

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ const DOM ={
     aboutbuttonmob : document.querySelector('#aboutbuttonmob'),
     projectbuttonmob : document.querySelector('#projectbuttonmob'),
     contactbuttonmob : document.querySelector('#contactbuttonmob'),
+
+    homebuttonCO : document.getElementById('homeCO'),
+    aboutbuttonCO : document.getElementById('aboutCO'),
+    projectbuttonCO : document.getElementById('projectCO'),
+    contactbuttonCO : document.getElementById('contactCO'),
 }
 
 
@@ -24,55 +29,150 @@ function homeSectionF(){
       });
       mobilemenu();
 }
+
+function homewithout(){
+    window.scrollTo( {
+        top : 0,
+        left : 0,
+        behavior: 'smooth'
+      });
+}
+
 DOM.homebutton.addEventListener('click', homeSectionF);  
 DOM.homebuttonmob.addEventListener('click', homeSectionF); 
+DOM.homebuttonCO.addEventListener('click', homewithout); 
 
 
 function aboutSectionF(){
-    window.scrollTo( {
-        top : 600,
-        left : 0,
-        behavior: 'smooth'
-      });
-      mobilemenu();
+    if (window.innerWidth > 700) {
+        window.scrollTo( {
+            top : 600,
+            left : 0,
+            behavior: 'smooth'
+          });
+    } else {
+        window.scrollTo( {
+            top : 840,
+            left : 0,
+            behavior: 'smooth'
+          });
+    }
+    mobilemenu();
 }
+
+function aboutwithout(){
+    if (window.innerWidth > 700) {
+        window.scrollTo( {
+            top : 600,
+            left : 0,
+            behavior: 'smooth'
+          });
+    } else {
+        window.scrollTo( {
+            top : 840,
+            left : 0,
+            behavior: 'smooth'
+          });
+    }
+}
+
 DOM.aboutbutton.addEventListener('click', aboutSectionF);  
 DOM.aboutbuttonmob.addEventListener('click', aboutSectionF);  
-DOM.homeaboutbutton.addEventListener('click', aboutSectionF);  
+DOM.aboutbuttonCO.addEventListener('click', aboutwithout);
+DOM.homeaboutbutton.addEventListener('click', aboutwithout);  
 
 
 function projectSectionF(){
-    window.scrollTo( {
-        top : 1200,
-        left : 0,
-        behavior: 'smooth'
-      });
-      mobilemenu();
+    if (window.innerWidth > 700) {
+        window.scrollTo( {
+            top : 1200,
+            left : 0,
+            behavior: 'smooth'
+          });
+    } else {
+        window.scrollTo( {
+            top : 1500,
+            left : 0,
+            behavior: 'smooth'
+          });
+    }
+    mobilemenu();
 }
+
+function projectwithout(){
+    if (window.innerWidth > 700) {
+        window.scrollTo( {
+            top : 1200,
+            left : 0,
+            behavior: 'smooth'
+          });
+    } else {
+        window.scrollTo( {
+            top : 1500,
+            left : 0,
+            behavior: 'smooth'
+          });
+    }
+}
+
 DOM.projectbutton.addEventListener('click', projectSectionF);
 DOM.projectbuttonmob.addEventListener('click', projectSectionF);
-
+DOM.projectbuttonCO.addEventListener('click', projectwithout);
 
 function contactSectionF(){
+    if (window.innerWidth > 700) {
     window.scrollTo( {
         top : 1800,
         left : 0,
         behavior: 'smooth'
       });
-      mobilemenu();
+    } else {
+        window.scrollTo( {
+            top : 2850,
+            left : 0,
+            behavior: 'smooth'
+          });
+    }
+    mobilemenu();
 }
+
+function contactwithout(){
+    if (window.innerWidth > 700) {
+    window.scrollTo( {
+        top : 1800,
+        left : 0,
+        behavior: 'smooth'
+      });
+    } else {
+        window.scrollTo( {
+            top : 2850,
+            left : 0,
+            behavior: 'smooth'
+          });
+    }
+}
+
 DOM.contactbutton.addEventListener('click', contactSectionF);
 DOM.contactbuttonmob.addEventListener('click', contactSectionF);
+DOM.contactbuttonCO.addEventListener('click', contactwithout);
 
 
-
-
-function mobilemenu(){
+function startmenu(){
     DOM.menu.classList.toggle('head_menu_mobile_close');
     DOM.homebuttonmob.classList.toggle('head_menu_section_close');
     DOM.aboutbuttonmob.classList.toggle('head_menu_section_close');
     DOM.projectbuttonmob.classList.toggle('head_menu_section_close');
     DOM.contactbuttonmob.classList.toggle('head_menu_section_close');
 }
+function mobilemenu(){
+    DOM.menu.classList.toggle('head_menu_mobile_close');
+    DOM.homebuttonmob.classList.toggle('head_menu_section_close');
+    DOM.aboutbuttonmob.classList.toggle('head_menu_section_close');
+    DOM.projectbuttonmob.classList.toggle('head_menu_section_close');
+    DOM.contactbuttonmob.classList.toggle('head_menu_section_close');
+
+    DOM.menubutton.classList.toggle('opened');
+    DOM.menubutton.setAttribute('aria-expanded', DOM.menubutton.classList.contains('opened'));
+}
 DOM.menubutton.addEventListener('click', mobilemenu);
-window.addEventListener('load', mobilemenu);
+window.addEventListener('load', startmenu);
